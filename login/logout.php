97b939276eb30afb2a5ac960c51ab188c47cf904
Unai-193 <unaix193@gmail.com>
2025-01-17 18:41:23 +0100

    Trabajando desde casa se ha cambiado el logout para borrar todos los datos de sesion y quitar el token que al final no se va a hacer

--- login/logout.php
+++ login/logout.php
@@ -1,6 +1,7 @@
 <?php
-
-setcookie("token", "", time() - 3600, "/", "", true, true);
-header('location:index.php');
-
+session_start();
+session_unset();
+session_destroy();
+header("Location: index.php");
+exit();
 ?>
